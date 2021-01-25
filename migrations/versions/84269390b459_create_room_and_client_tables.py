7dd87dc76c0c1578d1bfa00d91b6b138da935b61
Morgan Robertson <mrgnr@pm.me>
2021-01-25 12:35:52 +0100

    wip - migrations

---
+++ migrations/versions/84269390b459_create_room_and_client_tables.py
@@ -0,0 +1,24 @@
+"""create room and client tables
+
+Revision ID: 84269390b459
+Revises: 
+Create Date: 2021-01-16 14:50:33.422466+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '84269390b459'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
